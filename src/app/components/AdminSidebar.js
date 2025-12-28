5e50490fe3703771e919ba0dbd1a9a71e0f7cc9e
unknown <nazmiefg400@gmail.com>
2025-12-28 18:53:35 +0700

    update admin

--- src/app/components/AdminSidebar.js
+++ src/app/components/AdminSidebar.js
@@ -17,7 +17,7 @@ export default function Sidebar() {
     },
     { 
       name: 'Sekolah', 
-      path: '/Admin/sekolah/tambah-sekolah', 
+      path: '/Admin/sekolah', 
       icon: (
         <svg className="w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20">
           <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/>
