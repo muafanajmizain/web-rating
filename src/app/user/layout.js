57f3f3f4d2c2378b4de599b4aa37cbb55331d96c
mayopi <cyade007@gmail.com>
2026-01-20 07:59:19 +0700

    fix: AuthGuard publicly accessible on /user pages

--- src/app/user/layout.js
+++ src/app/user/layout.js
@@ -1,20 +1,15 @@
 // src/app/user/layout.js
-"use client";
-
-import AuthGuard from "@/components/AuthGuard";
 import UserHeader from "../components/UserHeader";
 import UserFooter from "../components/UserFooter";
 
 export default function UserRootLayout({ children }) {
   return (
-    <AuthGuard allowedRoles={["user"]}>
-      <div className="min-h-screen bg-white flex flex-col">
-        <UserHeader />
+    <div className="min-h-screen bg-white flex flex-col">
+      <UserHeader />
 
-        <main className="flex-grow">{children}</main>
+      <main className="flex-grow">{children}</main>
 
-        <UserFooter />
-      </div>
-    </AuthGuard>
+      <UserFooter />
+    </div>
   );
 }
